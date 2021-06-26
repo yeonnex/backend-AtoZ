1530cc05440d94b5f1f19868ad692d1ad823917a
yeonnex <yeonnex@gmail.com>
2021-06-27 08:56:54 +0900

    html컨텐츠 보내주기-1

--- simple-node-server/1-http.js
+++ simple-node-server/1-http.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const { CLIENT_RENEG_LIMIT } = require('tls');
 // const http = require('http2'); // https
 
 const server = http.createServer((req, res)=>{
@@ -8,7 +7,26 @@ const server = http.createServer((req, res)=>{
     console.log(req.httpVersion);
     console.log(req.method);
     console.log(req.url);
-    res.write('Welcome!');
+    const url = req.url
+    if (url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Academy</title></head>')
+        res.write('<body><h1>Welcome!!!</h1></body>')
+        res.write('</html>')
+    }else if (url === '/courses'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Academy</title></head>')
+        res.write('<body><h1>Course Page</h1></body>')
+        res.write('</html>')
+    }else{
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Academy</title></head>')
+        res.write('<body><h1>Not Found...</h1></body>')
+        res.write('</html>')
+    }
     res.end()
 })
 
