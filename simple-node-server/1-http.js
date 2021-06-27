80c8deef1a172b98166569eb461b22927e60f0f4
yeonnex <yeonnex@gmail.com>
2021-06-27 09:18:21 +0900

    html 컨텐츠 보내주기 -1

--- simple-node-server/1-http.js
+++ simple-node-server/1-http.js
@@ -1,33 +1,22 @@
 const http = require('http');
-// const http = require('http2'); // https
+const fs = require('fs');
+// const http2 = require('http2'); // https
 
-const server = http.createServer((req, res)=>{
-    console.log('incomming...')
-    console.log(req.headers);
-    console.log(req.httpVersion);
-    console.log(req.method);
-    console.log(req.url);
-    const url = req.url
-    if (url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>')
-        res.write('<head><title>Academy</title></head>')
-        res.write('<body><h1>Welcome!!!</h1></body>')
-        res.write('</html>')
-    }else if (url === '/courses'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>')
-        res.write('<head><title>Academy</title></head>')
-        res.write('<body><h1>Course Page</h1></body>')
-        res.write('</html>')
-    }else{
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>')
-        res.write('<head><title>Academy</title></head>')
-        res.write('<body><h1>Not Found...</h1></body>')
-        res.write('</html>')
-    }
-    res.end()
-})
+const server = http.createServer((req, res) => {
+  console.log('incoming...');
+  console.log(req.headers);
+  console.log(req.httpVersion);
+  console.log(req.method);
+  console.log(req.url);
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/html');
+  if (url === '/') {
+    fs.createReadStream('./html/index.html').pipe(res);
+  } else if (url === '/courses') {
+    fs.createReadStream('./html/courses.html').pipe(res);
+  } else {
+    fs.createReadStream('./html/not-found.html').pipe(res);
+  }
+});
 
 server.listen(8080);
